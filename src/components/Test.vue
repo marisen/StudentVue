410cb12f3b85106f1ca7b16080fdcbfaaf1e86d6
marisen <787967891@qq.com>
2019-12-30 15:40:38 +0800

    学习vue的项目

---
+++ src/components/Test.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2 v-text="msg"></h2>
+    <h3 v-pre>{{msg}}</h3>
+    <WodevueVue :title="t1" @listenToChild="show"/>
+  </div>
+</template>
+
+<script>
+import WodevueVue from './Wodevue.vue'
+export default {
+  name: 'TestVue',
+  data () {
+    return {
+      msg: '在测试我的VUE',
+      t1:'父子传值'
+    }
+  },
+  components:{
+    WodevueVue
+  },methods: {
+    show(name){
+      console.log(name);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Wodevue.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <h2>{{title}}</h2>
+    <input type="text" name="" id="" v-model="id">
+    <input type="text" name="" id="" v-model="name">
+    <input type="button" value="添加" @click="Add">
+    <p v-for="(item,i) in historyList" :key="item.id"><input type="checkbox">索引：{{i}}------{{item.id}}-----{{item.name|StringFormat}}</p>
+</div>
+</template>
+<script>
+
+export default {
+    name:"wode",
+    data(){
+        return{
+            id:"",
+            name:"",
+            historyList:[
+                   {id:1,name:"秦"},
+                   {id:2,name:"汉"},
+                   {id:3,name:"晋"},
+                   {id:4,name:"隋"}
+               ]
+        }
+    },
+    methods: {
+        Add(){
+            this.historyList.push({id:this.id,name:this.name})
+            this.$emit("listenToChild",this.name);
+        }
+    },
+    filters: {
+        StringFormat:val=>{
+            return val+"777";
+        }
+    },
+    props: ["title"]
+}
+</script>
